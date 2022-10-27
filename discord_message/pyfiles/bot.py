8ddbb5d1ec2c357dae77dd69aaf05285e40da83c
Andrino Machado Cauduro <116812119+AndrinoC@users.noreply.github.com>
2022-10-27 10:43:47 -0300

    Updated to 0.6

--- discord_message/pyfiles/bot.py
+++ discord_message/pyfiles/bot.py
@@ -1,7 +1,7 @@
 '''
 Author: Andrino Machado Cauduro
 Date: 2022-10-25
-Version: 0.5
+Version: 0.6
 About: Annoy your friends
 '''
 
@@ -34,7 +34,6 @@ while True:
     if events == sg.WINDOW_CLOSED:
         quit()
     if events == 'Send':     
-        pc.copy(values['text']) ##Copying text to be sent, that way it will work with special characters
         mx.init() ##Starting pygame mixer
         mx.music.load(".\\resources\\sfx\\go.wav")
         mx.music.play()
@@ -47,12 +46,14 @@ while True:
                 coord = auto.locateCenterOnScreen(".\\resources\\image_detection\\"+img[i])
                 auto.moveTo(coord)
                 auto.click()
-        auto.write(values['target']) ##Searching target on friendlist
+        pc.copy(values['target']) ##Copying target to be sent, that way it will work with special characters
+        auto.hotkey("ctrl", "v") 
         time.sleep(1)
         auto.press('tab') ##Opening target's chat
         auto.press('enter')
         time.sleep(1)
-        auto.hotkey("ctrl", "v") ##Pasting text
+        pc.copy(values['text']) 
+        auto.hotkey("ctrl", "v") 
         auto.press('enter')
         time.sleep(1)
         pc.copy('') ##Clearing clipboard
